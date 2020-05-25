11e7960c1ec7ab59546d1ab452d7cba77efe20a4
${wanghao980519} <${wanghao@triumen.com}>
2020-05-25 14:58:14 +0800

    新建项目

---
+++ src/main/java/com/wh/SpringbootWhPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.wh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWhPractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWhPractiseApplication.class, args);
+    }
+
+}
